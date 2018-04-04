79f68de0d9d66478343f52e483afd0891e1dfa8f
Leigh Tarasenko <leigh.tarasenko@rea-group.com>
2018-04-04 11:42:57 +1000

    Speed up mac keyrepeat speed

--- osx_install.sh
+++ osx_install.sh
@@ -8,3 +8,6 @@ brew bundle
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 xcode-select --install
+
+defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 2 # normal minimum is 2 (30 ms)
